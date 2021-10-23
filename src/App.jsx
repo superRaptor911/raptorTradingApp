949fc127aad7fd538c2e0032334f273ffc8b158f
superRaptor911 <raptor.inc2018@gmail.com>
2021-10-23 12:08:36 +0530

    Header test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Header from './components/Header';
 import Home from './pages/Home';
 import {ROUTES} from './routes';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path={ROUTES.home}>
           <Home />
