bd1e9febb54e2523ee611544e137aee5d48ce9a9
superRaptor911 <raptor.inc2018@gmail.com>
2022-02-15 17:23:20 +0530

    created toml for netlify

--- vite.config.js
+++ vite.config.js
@@ -11,15 +11,26 @@ export default defineConfig({
         enabled: true,
       },
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.png', 'robots.txt'],
+      includeAssets: [
+        'favicon.png',
+        'robots.txt',
+        'apple-touch-icon.png',
+        'icons/*.svg',
+      ],
       manifest: {
         theme_color: '#BD34FE',
+
         icons: [
           {
-            src: 'favicon.png',
-            sizes: '512x512',
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
           },
         ],
       },
